836a54d71882ad3ec63adcaf92d1bfc80a856bcf
Kang <jobo.zh@gmail.com>
2009-07-12 14:17:17 +0800

    Add the support for user created keyring implementation.

--- keyring/__init__.py
+++ keyring/__init__.py
@@ -1,5 +1,5 @@
+__all__ = [ 'backend', ]
 
 from _keyring import *
-
 
 
--- keyring/_keyring.py
+++ keyring/_keyring.py
@@ -15,9 +15,10 @@ def setpass(service_name,username,password):
 
 def _init_backend():
     # select a backend according to the config file
-    keyring = _load_config()
+    keyring_impl = _load_config()
 
-    if keyring is None:
+    # if the user dose not specify a keyring, we apply a default one
+    if keyring_impl is None:
         # TODO set keyring to pure python implementation
 
         # select a default backend for the platform
@@ -27,43 +28,71 @@ def _init_backend():
         if platform in ['darwin','mac']:
             # for Mac OSX 
             from backend import OSXKeychain
-            keyring = OSXKeychain()
+            keyring_impl = OSXKeychain()
         else:
             # detect KDE or Gnome
             import os
             if os.getenv("KDE_FULL_SESSION") == "true":
                 # KDE enviroment
                 from backend import KDEKWallet 
-                keyring = KDEKWallet()
+                keyring_impl = KDEKWallet()
             elif os.getenv("GNOME_DESKTOP_SESSION_ID"):
                 # Gnome enviroment
                 from backend import GnomeKeyring
-                keyring = GnomeKeyring()
+                keyring_impl = GnomeKeyring()
 
-    return keyring 
+    return keyring_impl 
 
 def _load_config():
-    keyring = None
-    # search from current working directory
     import os,ConfigParser
-    keyring_cfg = os.path.join(os.getcwd(),".keyringrc")
-    if not os.path.exists(keyring_cfg):
-        #search the user's home folder
-        home = os.getenv("HOME")
-        keyring_cfg = os.path.join(home,".keyringrc")
+
+    keyring_impl = None
+
+    # search from current working directory
+    keyring_cfg_list = [os.path.join(os.getcwd(),".keyringrc"),
+                        os.path.join(os.getenv("HOME"),".keyringrc")]
+    keyring_cfg = None
+    for path in keyring_cfg_list:
+        keyring_cfg = path
+        if os.path.exists(path):
+            break
 
     if os.path.exists(keyring_cfg):
         config = ConfigParser.RawConfigParser()
         config.read(keyring_cfg)
+        # load the keyring-path option 
+        try: keyring_path = config.get("backend","keyring-path").strip()
+        except ConfigParser.NoOptionError: keyring_path = None
+        # load the keyring class name, and load it
         try:
             keyring_name = config.get("backend","default-keyring").strip()
-            exec "from backend import " + keyring_name 
-            exec "keyring = " + keyring_name + "()"
+
+            import imp
+            def find_module(name, path):
+                pl = name.split('.')
+                fp, pathname, description = imp.find_module(pl[0],path)
+                module = imp.load_module(pl[0], fp, pathname, description)
+                #print module.__path__
+                if len(pl) > 1:
+                    # for the class name containing dots
+                    sub_name = '.'.join(pl[1:])
+                    sub_path = path
+
+                    try: sub_path = module.__path__ 
+                    except AttributeError: return module
+
+                    return find_module(sub_name,sub_path)
+                return module
+
+            module = find_module( keyring_name, keyring_path)
+            keyring_class = keyring_name.split('.')[-1]
+
+            exec "keyring_impl = module." + keyring_class + "()" in locals()
         except ConfigParser.NoOptionError,ImportError:
             print "Keyring Config file does not write correctly.\n" + \
                   "Config file: %s" % keyring_cfg
     
-    return keyring 
+    return keyring_impl
 
 _keyring_backend = _init_backend()
 
