4b661c4be2434ed56d95e8b52a25c2375b5cd983
Jason R. Coombs <jaraco@jaraco.com>
2010-09-12 09:08:43 -0400

    Just use getpass module, which is portable and does the same thing keyring tried to do.

--- keyring/getpassbackend.py
+++ keyring/getpassbackend.py
@@ -1,20 +1,10 @@
 """Specific support for getpass."""
 import os
-import pwd
+import getpass
 
 from keyring.core import get_password as original_get_password
 
-def get_default_user():
-    """Get the username from the environment or password database.
-
-    First try various environment variables, then the password
-    database.  This works on Windows as long as USERNAME is set.
-    """
-    for name in ('LOGNAME', 'USER', 'LNAME', 'USERNAME'):
-        user = os.environ.get(name)
-        if user:
-            return user
-    return pwd.getpwuid(os.getuid())[0]
+get_default_user = getpass.getuser
 
 def get_password(prompt='Password: ', stream=None,
                            service_name='Python',
