c1d34534b8d66685e8233c4d3003e9aa8d8370c3
Jason R. Coombs <jaraco@jaraco.com>
2011-03-25 09:09:19 -0400

    Fixed indentation

--- keyring/getpassbackend.py
+++ keyring/getpassbackend.py
@@ -7,7 +7,7 @@ from keyring.core import get_password as original_get_password
 get_default_user = getpass.getuser
 
 def get_password(prompt='Password: ', stream=None,
-                           service_name='Python',
-                           username=get_default_user()):
+                 service_name='Python',
+                 username=get_default_user()):
     return original_get_password(service_name, username)
 
