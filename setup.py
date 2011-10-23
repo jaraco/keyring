1b82f80a9041dd6e4c522ec67ec7e4bfd3afcedb
Jason R. Coombs <jaraco@jaraco.com>
2011-10-23 04:37:43 -0400

    Refactored setup.py so it's importable

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ Setup the Keyring Lib for Python.
 """
 
 import sys, os, subprocess
-from distutils.core import setup
 from distutils.version import StrictVersion
 
 def runcmd(cmd, env):
@@ -16,26 +15,35 @@ def runcmd(cmd, env):
     out, err = p.communicate()
     return out, err
 
-if sys.platform == 'darwin' and os.path.exists('/usr/bin/xcodebuild'):
-    # XCode 4.0 dropped support for ppc architecture, which is hardcoded in
-    # distutils.sysconfig
+def patch_xcode():
+    """
+    XCode 4.0 dropped support for ppc architecture, which is hardcoded in
+    distutils.sysconfig
+    """
+    if sys.platform != 'darwin' or not os.path.exists('/usr/bin/xcodebuild'):
+        return
     version = runcmd(['/usr/bin/xcodebuild', '-version'], {})[0].splitlines()[0]
     # Also parse only first digit, because 3.2.1 can't be parsed nicely
     if (version.startswith('Xcode') and
         StrictVersion(version.split()[1]) >= StrictVersion('4.0')):
         os.environ['ARCHFLAGS'] = ''
 
-setup(name = 'keyring',
-      version = "0.7",
-      description = "Store and access your passwords safely.",
-      url = "http://home.python-keyring.org/",
-      keywords = "keyring Keychain GnomeKeyring Kwallet password storage",
-      maintainer = "Kang Zhang",
-      maintainer_email = "jobo.zh@gmail.com",
-      license="PSF",
-      long_description = open('README').read() + open('CHANGES.txt').read(),
-      platforms = ["Many"],
-      packages = ['keyring', 'keyring.tests', 'keyring.util',
-                  'keyring.backends'],
-    )
+setup_params = dict(
+    name = 'keyring',
+    version = "0.7",
+    description = "Store and access your passwords safely.",
+    url = "http://home.python-keyring.org/",
+    keywords = "keyring Keychain GnomeKeyring Kwallet password storage",
+    maintainer = "Kang Zhang",
+    maintainer_email = "jobo.zh@gmail.com",
+    license="PSF",
+    long_description = open('README').read() + open('CHANGES.txt').read(),
+    platforms = ["Many"],
+    packages = ['keyring', 'keyring.tests', 'keyring.util',
+                'keyring.backends'],
+)
 
+if __name__ == '__main__':
+    from distutils.core import setup
+    patch_xcode()
+    setup(**setup_params)
