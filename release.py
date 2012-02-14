54eed97a4cae09e1e1211f7e5d93f154eebb2f3b
Jason R. Coombs <jaraco@jaraco.com>
2012-02-14 16:22:58 -0700

    Added missing import

--- release.py
+++ release.py
@@ -5,6 +5,7 @@ release.py - releases keyring to the cheeseshop
 """
 
 import subprocess
+import sys
 
 # we only upload a source distribution now as there are no C extensions
 subprocess.Popen([sys.executable, 'setup.py', 'sdist', 'upload'])
