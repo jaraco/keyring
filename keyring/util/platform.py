6a2794823511e29fc7193965ab7f9741a3d3fff0
Jason R. Coombs <jaraco@jaraco.com>
2012-02-15 10:55:41 -0700

    Use platform module to determine platform for data_root

--- keyring/util/platform.py
+++ keyring/util/platform.py
@@ -1,10 +1,17 @@
 import os
 import sys
 
-def _data_root_win32():
+# While we support Python 2.4, use a convoluted technique to import
+#  platform from the stdlib.
+# With Python 2.5 or later, just do "from __future__ import absolute_import"
+#  and "import platform"
+exec('__import__("platform", globals=dict())')
+platform = sys.modules['platform']
+
+def _data_root_Windows():
 	return os.path.join(os.environ['LOCALAPPDATA'], 'Python Keyring')
 
-def _data_root_linux2():
+def _data_root_Linux():
 	"""
 	Use freedesktop.org Base Dir Specfication to determine storage
 	location.
@@ -14,4 +21,4 @@ def _data_root_linux2():
 	return os.path.join(root, 'python_keyring')
 
 # by default, use Unix convention
-data_root = globals().get('_data_root_' + sys.platform, _data_root_linux2)
+data_root = globals().get('_data_root_' + platform.system(), _data_root_Linux)
