18f35c91ec66d040164394868f29f159f23d865d
Jason R. Coombs <jaraco@jaraco.com>
2013-02-10 06:55:44 -0500

    Bumped to 1.2.3 in preparation for next release.

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ if sys.version_info >= (3, 0):
 
 setup_params = dict(
     name = 'keyring',
-    version = "1.2.2",
+    version = "1.2.3",
     description = "Store and access your passwords safely.",
     url = "http://bitbucket.org/kang/python-keyring-lib",
     keywords = "keyring Keychain GnomeKeyring Kwallet password storage",
