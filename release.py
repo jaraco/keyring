d55b95d5bb0df3f8a7ac21c562c053bbf54d6879
Jason R. Coombs <jaraco@jaraco.com>
2013-05-25 08:43:44 -0400

    Oops - version number was bumped with keyring version bumps

--- release.py
+++ release.py
@@ -70,7 +70,7 @@ def add_milestone_and_version(version=NEXT_VERSION):
 		}
 	base = 'https://api.bitbucket.org'
 	for type in 'milestones', 'versions':
-		url = (base + '/1.2/repositories/{repo}/issues/{type}'
+		url = (base + '/1.0/repositories/{repo}/issues/{type}'
 			.format(repo = get_repo_name(), type=type))
 		req = urllib2.Request(url = url, headers = headers,
 			data='name='+version)
