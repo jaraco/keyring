7a7834f40c8474c481fb1d21e1bac5b204f8f7bc
Jason R. Coombs <jaraco@jaraco.com>
2013-06-23 23:22:27 -0400

    Fix SecretService viability tests
    --HG--
    branch : prioritized backends

--- keyring/tests/backends/test_SecretService.py
+++ keyring/tests/backends/test_SecretService.py
@@ -3,7 +3,7 @@ import sys
 from ..py30compat import unittest
 from ..test_backend import BackendBasicTests
 from keyring.backends import SecretService
-from ..util import ImportKiller
+from .. import util
 
 @unittest.skipUnless(SecretService.Keyring.viable,
     "SecretStorage package is needed for SecretServiceKeyring")
@@ -18,14 +18,14 @@ class SecretServiceKeyringTestCase(BackendBasicTests, unittest.TestCase):
 class SecretServiceKeyringUnitTests(unittest.TestCase):
     def test_supported_no_dbus(self):
         """
-        SecretService Keyring is not supported if dbus can't be imported.
+        SecretService Keyring is not viable if dbus can't be imported.
         """
-        with ImportKiller('dbus'):
-            self.assertEqual(-1, SecretService.Keyring().supported())
+        with util.ImportKiller('dbus'):
+            self.assertFalse(SecretService.Keyring.viable)
 
     def test_supported_no_secretstorage(self):
         """
-        SecretService Keyring is not supported if secretstorage can't be imported.
+        SecretService Keyring is not viable if secretstorage can't be imported.
         """
-        with ImportKiller('secretstorage'):
-            self.assertEqual(-1, SecretService.Keyring().supported())
+        with util.NoNoneDictMutator(SecretService.__dict__, secretstorage=None):
+            self.assertFalse(SecretService.Keyring.viable)
