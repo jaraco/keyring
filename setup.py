48c73360fa1b8e348550c0d828956e6b2061c440
Jason R. Coombs <jaraco@jaraco.com>
2013-08-26 21:54:19 -0400

    Bumped to 2.0.3 in preparation for next release.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ pytest_runner = ['pytest-runner'] if 'ptr' in sys.argv else []
 
 setup_params = dict(
     name = 'keyring',
-    version = "2.0.2",
+    version = "2.0.3",
     description = "Store and access your passwords safely.",
     url = "http://bitbucket.org/kang/python-keyring-lib",
     keywords = "keyring Keychain GnomeKeyring Kwallet password storage",
