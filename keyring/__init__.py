089b5b834c63b4dbe112e40014ef51a38f0be3f1
Jason R. Coombs <jaraco@jaraco.com>
2013-08-31 21:18:06 -0400

    Use absolute import in keyring module

--- keyring/__init__.py
+++ keyring/__init__.py
@@ -3,9 +3,12 @@ __init__.py
 
 Created by Kang Zhang on 2009-07-09
 """
+
+from __future__ import absolute_import
+
 import logging
 logger = logging.getLogger('keyring')
 
-from core import (set_keyring, get_keyring, set_password, get_password,
+from .core import (set_keyring, get_keyring, set_password, get_password,
                   delete_password)
 from keyring.getpassbackend import get_password as get_pass_get_password
