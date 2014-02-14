01bfe58825166978c3aaec475c2662f7928b0a7e
Jason R. Coombs <jaraco@jaraco.com>
2014-02-14 15:09:15 -0500

    Add support for XDG_CONFIG_HOME in _platform module. Ref #99. This functionality won't be enabled until the 4.0 release
    --HG--
    extra : amend_source : 75bfe83d2ab43dff8f8eb80a3a829cc03548860d

--- keyring/util/platform_.py
+++ keyring/util/platform_.py
@@ -23,5 +23,17 @@ def _data_root_Linux():
 	root = os.environ.get('XDG_DATA_HOME', None) or fallback
 	return os.path.join(root, 'python_keyring')
 
+_config_root_Windows = _data_root_Windows
+
+def _config_root_Linux():
+	"""
+	Use freedesktop.org Base Dir Specfication to determine config
+	location.
+	"""
+	fallback = os.path.expanduser('~/.local/share')
+	root = os.environ.get('XDG_CONFIG_HOME', None) or fallback
+	return os.path.join(root, 'python_keyring')
+
 # by default, use Unix convention
 data_root = globals().get('_data_root_' + platform.system(), _data_root_Linux)
+config_root = globals().get('_config_root' + platform.system(), _config_root_Linux)
