add20d920a9553ea9f6ed4c929874f83dc4c4f72
Jason R. Coombs <jaraco@jaraco.com>
2016-01-10 10:11:48 -0500

    Add tests on the popular names (and legacy names) to capture API guarantees. Ref #187.

--- keyring/core.py
+++ keyring/core.py
@@ -88,6 +88,26 @@ def _get_best_keyring():
 def _load_keyring_class(keyring_name):
     """
     Load the keyring class indicated by name.
+
+    These popular names are tested to ensure their presence.
+
+    >>> popular_names = [
+    ...      'keyring.backends.Windows.WinVaultKeyring',
+    ...      'keyring.backends.OS_X.Keyring',
+    ...      'keyring.backends.kwallet.DBusKeyring',
+    ...      'keyring.backends.SecretService.Keyring',
+    ...      'keyring.backends.Gnome.Keyring',
+    ...  ]
+    >>> list(map(_load_keyring_class, popular_names))
+    [...]
+
+    These legacy names are retained for compatibility.
+
+    >>> legacy_names = [
+    ...      'keyring.backends.kwallet.Keyring',
+    ...  ]
+    >>> list(map(_load_keyring_class, legacy_names))
+    [...]
     """
     module_name, sep, class_name = keyring_name.rpartition('.')
     __import__(module_name)
