b51367b6f39fe14c175c80983fe086771d16c158
Jason R. Coombs <jaraco@jaraco.com>
2016-01-11 10:16:07 -0500

    Spaces for indent

--- conftest.py
+++ conftest.py
@@ -1,8 +1,8 @@
 import platform
 
 collect_ignore = [
-	"hook-keyring.backend.py",
+    "hook-keyring.backend.py",
 ]
 
 if platform.system() != 'Windows':
-	collect_ignore.append('keyring/backends/_win_crypto.py')
+    collect_ignore.append('keyring/backends/_win_crypto.py')
--- keyring/tests/py30compat.py
+++ keyring/tests/py30compat.py
@@ -5,6 +5,6 @@ no longer required.
 import sys
 
 if sys.version_info < (2,7) or sys.version_info[:2] == (3,0):
-	import unittest2 as unittest
+    import unittest2 as unittest
 else:
-	import unittest
+    import unittest
