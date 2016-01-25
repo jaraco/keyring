407e8f2a46d835677e82f9b1b99232f50e65b7f5
Jason R. Coombs <jaraco@jaraco.com>
2016-01-25 10:14:48 -0500

    Correct error message to better communicate what is missing. Ref #196.

--- keyring/backends/fail.py
+++ keyring/backends/fail.py
@@ -9,12 +9,12 @@ class Keyring(KeyringBackend):
     >>> kr.get_password('svc', 'user')
     Traceback (most recent call last):
     ...
-    RuntimeError: ...
+    RuntimeError: ...No recommended backend...
     """
 
     priority = 0
 
     def get_password(self, service, username, password=None):
-        raise RuntimeError("No recommended password was available")
+        raise RuntimeError("No recommended backend was available")
 
     set_password = delete_pasword = get_password
