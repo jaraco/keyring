5e43eb700c8d53880705516930f22b2e752c48a4
Jason R. Coombs <jaraco@jaraco.com>
2016-10-20 13:18:26 -0400

    Add test capturing failure. Ref #247.

---
+++ tests/test_packaging.py
@@ -0,0 +1,14 @@
+import pkg_resources
+
+def test_entry_point():
+    """
+    Keyring provides exactly one 'keyring' console script
+    that's a callable.
+    """
+    eps = pkg_resources.iter_entry_points('console_scripts')
+    ep, = (
+        ep
+        for ep in eps
+        if ep.name == 'keyring'
+    )
+    assert callable(ep.resolve())
