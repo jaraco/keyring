5c843155ef975d49e5d7c239ca8affaeed9e1de6
Jason R. Coombs <jaraco@jaraco.com>
2018-10-27 15:19:16 -0400

    Update __init__.py

--- keyring/__init__.py
+++ keyring/__init__.py
@@ -5,5 +5,5 @@ from .core import (set_keyring, get_keyring, set_password, get_password,
 
 __all__ = (
     'set_keyring', 'get_keyring', 'set_password', 'get_password',
-    'delete_password', 'get_username_and_password'
+    'delete_password', 'get_username_and_password',
 )
