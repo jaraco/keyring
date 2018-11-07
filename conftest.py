ec5ff925c7d58ed1e70581b46d62461fb54e2d98
Jason R. Coombs <jaraco@jaraco.com>
2018-11-07 17:56:50 -0500

    Remove old workaround

--- conftest.py
+++ conftest.py
@@ -8,14 +8,3 @@ if platform.system() != 'Darwin':
     collect_ignore.append('keyring/backends/_OS_X_API.py')
 
 collect_ignore.append('keyring/devpi_client.py')
-
-
-def pytest_configure():
-        workaround_sugar_issue_159()
-
-
-def workaround_sugar_issue_159():
-    "https://github.com/Frozenball/pytest-sugar/159"
-    import pytest_sugar
-    pytest_sugar.SugarTerminalReporter.pytest_runtest_logfinish = \
-        lambda self: None
