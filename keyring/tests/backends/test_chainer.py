a2400b6980089803b38121e20e2d24ee2f463eb1
Jason R. Coombs <jaraco@jaraco.com>
2018-11-08 20:57:21 -0500

    Add a test for the chainer.

---
+++ keyring/tests/backends/test_chainer.py
@@ -0,0 +1,37 @@
+import pytest
+
+import keyring.backends.chainer
+from keyring import backend
+
+
+@pytest.fixture
+def two_keyrings(monkeypatch):
+    def get_two():
+        class Keyring1(backend.KeyringBackend):
+            priority = 1
+
+            def get_password(self, system, user):
+                return 'ring1-{system}-{user}'.format(**locals())
+
+            def set_password(self, system, user, password):
+                pass
+
+        class Keyring2(backend.KeyringBackend):
+            priority = 2
+
+            def get_password(self, system, user):
+                return 'ring2-{system}-{user}'.format(**locals())
+
+            def set_password(self, system, user, password):
+                raise NotImplementedError()
+
+        return Keyring1(), Keyring2()
+
+    monkeypatch.setattr('keyring.backend.get_all_keyring', get_two)
+
+
+class TestChainer:
+    def test_chainer_gets_from_highest_priority(self, two_keyrings):
+        chainer = keyring.backends.chainer.ChainerBackend()
+        pw = chainer.get_password('alpha', 'bravo')
+        assert pw == 'ring2-alpha-bravo'
