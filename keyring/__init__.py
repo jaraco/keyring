3d0db3844f76f883888cf065ad8e6980280b06dc
Jason R. Coombs <jaraco@jaraco.com>
2018-12-06 13:35:36 -0500

    Avoid hanging indent

--- keyring/__init__.py
+++ keyring/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
-from .core import (set_keyring, get_keyring, set_password, get_password,
-                   delete_password, get_credential)
+from .core import (
+    set_keyring, get_keyring, set_password, get_password,
+    delete_password, get_credential)
 
 __all__ = (
     'set_keyring', 'get_keyring', 'set_password', 'get_password',
