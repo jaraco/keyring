dda311f0057e981883a91ea493721289a5a6f154
Jason R. Coombs <jaraco@jaraco.com>
2019-01-26 13:06:43 -0500

    Try building with 'alabaster' theme

--- docs/conf.py
+++ docs/conf.py
@@ -27,5 +27,6 @@ link_files = {
 }
 
 # Custom sidebar templates, maps document names to template names.
+html_theme = 'alabaster'
 templates_path = ['_templates']
 html_sidebars = {'index': 'tidelift-sidebar.html'}
