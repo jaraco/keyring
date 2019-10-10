c7b5b62fdd3cc75937c744f480cf5cc3bc6fae0c
Jason R. Coombs <jaraco@jaraco.com>
2019-10-10 18:05:14 -0400

    Instead of generating an error message, extract the appropriate error message.

--- keyring/backends/_OS_X_API.py
+++ keyring/backends/_OS_X_API.py
@@ -35,79 +35,19 @@ SecKeychainCopyDefault.argtypes = (POINTER(sec_keychain_ref),)
 SecKeychainCopyDefault.restype = OS_status
 
 
-class CFRuntimeBase(ctypes.Structure):
-    _fields_ = [
-        ("_isa", c_void_p),  # uintptr_t
-        ("_info", c_uint32),  # uint8_t
-        ("_rc", c_uint32),  # uint32_t
-    ]
+class CFString(ctypes.Structure):
 
-
-class CFStringRefVariants(ctypes.Union):
-    class inline1(ctypes.Structure):
-        _fields_ = [
-            ("length", c_int32),
-            ("buffer", c_char_p),
-        ]
-
-    _fields_ = [
-        ("inline1", inline1)
-        # ...
-    ]
-
-
-class CFStringRef(ctypes.Structure):
-    _fields_ = [
-        ("base", CFRuntimeBase),
-        ("variants", CFStringRefVariants),
-    ]
-
-    @property
-    def inline(self):
-        contents_mask = 0x060
-        inline_contents = 0
-        return self.base._info & contents_mask == inline_contents
-
-    @property
-    def has_explicit_length(self):
+    def __str__(self):
         """
-        Has explicit length if (1) mutable or (2) not mutable
-        and no length byte.
+        Convert to a python string
         """
-        is_mutable_mask = 0x01
-        has_length_byte_mask = 0x04
-        has_length_byte = 0x04
-        mask = is_mutable_mask | has_length_byte_mask
-        return self.base._info & mask != has_length_byte
+        str_length = CFStringGetLength(self) + 1
+        str_buff = ctypes.create_string_buffer(str_length)
+        assert CFStringGetCString(self, str_buff, str_length, 0)
+        return str_buff.value.decode('ascii')
 
-    @property
-    def length(self):
-        if not self.inline or not self.has_explicit_length:
-            raise NotImplementedError()
-        return self.variants.inline1.length
 
-    @property
-    def mutable(self):
-        is_mutable_mask = 0x01
-        is_mutable = 0x01
-        return self.base._info & is_mutable_mask == is_mutable
-
-    @property
-    def has_length_byte(self):
-        has_length_byte_mask = 0x04
-        has_length_byte = 0x04
-        return self.base._info & has_length_byte_mask == has_length_byte
-
-    @property
-    def is_unicode(self):
-        is_unicode_mask = 0x10
-        is_unicode = 0x10
-        return self.base._info & is_unicode_mask == is_unicode
-
-    def contents(self):
-        assert self.inline, "Only inline supported"
-        assert self.has_explicit_length, "Only explicitl length supported"
-        return self.variants.inline1.buffer
+CFStringRef = POINTER(CFString)
 
 
 SecCopyErrorMessageString = _sec.SecCopyErrorMessageString
@@ -115,12 +55,17 @@ SecCopyErrorMessageString.argtypes = (OS_status, c_void_p)
 SecCopyErrorMessageString.restype = CFStringRef
 
 
+CFStringGetLength = _core.CFStringGetLength
+CFStringGetLength.argtypes = [CFStringRef]
+CFStringGetCString = _core.CFStringGetCString
+CFStringGetCString.argtypes = [CFStringRef, c_char_p, c_int32, c_int32]
+
+
 class Error(Exception):
     @classmethod
     def get_message(cls, status):
         ref = SecCopyErrorMessageString(status, None)
-        breakpoint()
-        msg = str()
+        msg = str(ref.contents)
         _core.CFRelease(ref)
         return msg
 
@@ -136,8 +81,7 @@ class Error(Exception):
             msg = cls.get_message(status)
             raise SecAuthFailure(
                 status,
-                "Security Auth Failure: make sure "
-                "python is signed with codesign util",
+                "Security Auth Failure: " + msg,
             )
         raise cls(status, "Unknown Error")
 
