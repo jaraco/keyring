0bd73db8a014e47080b3490972e4924dd3cb0110
jnsnow <john.n.snow@gmail.com>
2020-05-25 14:24:14 -0400

    Use correct XDG default for XDG_CONFIG_HOME
    The default for when XDG_CONFIG_HOME is not defined should be
    ~/.config and not ~/.local/share. This creates another backwards
    compatibility break, but one that _check_old_config_root already
    handles.

--- keyring/util/platform_.py
+++ keyring/util/platform_.py
@@ -56,7 +56,7 @@ def _config_root_Linux():
     location.
     """
     _check_old_config_root()
-    fallback = os.path.expanduser('~/.local/share')
+    fallback = os.path.expanduser('~/.config')
     key = 'XDG_CONFIG_HOME'
     root = os.environ.get(key, None) or fallback
     return os.path.join(root, 'python_keyring')
