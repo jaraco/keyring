2417fdba62443c1dc09e2327b4ad53e9e3c13e62
Francesco Truzzi <francesco@truzzi.me>
2020-12-30 01:10:04 +0100

    add test for reading UTF-8 password

--- tests/backends/test_Windows.py
+++ tests/backends/test_Windows.py
@@ -21,8 +21,45 @@ class TestWinVaultKeyring(BackendBasicTests):
     def init_keyring(self):
         return keyring.backends.Windows.WinVaultKeyring()
 
+    def set_utf8_password(self, service, username, password):
+        """
+        Write a UTF-8 encoded password using win32ctypes primitives
+        """
+        from win32ctypes.core import _authentication as auth
+        from win32ctypes.core.ctypes._common import LPBYTE
+        from ctypes import cast, c_char, create_string_buffer, sizeof
+
+        credential = dict(
+            Type=1,
+            TargetName=service,
+            UserName=username,
+            CredentialBlob=password,
+            Comment="Stored using python-keyring",
+            Persist=3,
+        )
+
+        c_cred = auth.CREDENTIAL.fromdict(credential, 0)
+        blob_data = create_string_buffer(password.encode("utf-8"))
+        c_cred.CredentialBlobSize = sizeof(blob_data) - sizeof(c_char)
+        c_cred.CredentialBlob = cast(blob_data, LPBYTE)
+        c_cred_pointer = auth.PCREDENTIAL(c_cred)
+        auth._CredWrite(c_cred_pointer, 0)
+
+        self.credentials_created.add((service, username))
+
     def test_long_password_nice_error(self):
         self.keyring.set_password('system', 'user', 'x' * 512 * 2)
+
+    def test_read_utf8_password(self):
+        """
+        Write a UTF-8 encoded credential and make sure it can be read back correctly.
+        """
+        service = "keyring-utf8-test"
+        username = "keyring"
+        password = "utf8-test"
+
+        self.set_utf8_password(service, username, password)
+        assert self.keyring.get_password(service, username) == password
 
 
 @pytest.mark.skipif('sys.platform != "win32"')
