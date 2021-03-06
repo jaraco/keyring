9a73294f7b9bdee815f2f7f429f0345726126724
Jason R. Coombs <jaraco@jaraco.com>
2021-03-05 20:41:31 -0500

    Fix monkeypatch argument in test_packaging. Fixes #498.

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -17,5 +17,5 @@ def test_missing_metadata(monkeypatch):
     """
     _load_plugins should pass when keyring metadata is missing.
     """
-    monkeypatch.setattr(metadata, 'entry_points', metadata.EntryPoints)
+    monkeypatch.setattr(metadata, 'entry_points', metadata.EntryPoints().select)
     backend._load_plugins()
