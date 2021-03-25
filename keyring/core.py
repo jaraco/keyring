5b786984fcf229d3081c767e6aadc5f63e33baea
Jason R. Coombs <jaraco@jaraco.com>
2021-03-25 09:06:18 -0400

    Refine language in TypeError. Fixes #504.

--- keyring/core.py
+++ keyring/core.py
@@ -22,7 +22,7 @@ def set_keyring(keyring):
     """Set current keyring backend."""
     global _keyring_backend
     if not isinstance(keyring, backend.KeyringBackend):
-        raise TypeError("The keyring must be a subclass of KeyringBackend")
+        raise TypeError("The keyring must be an instance of KeyringBackend")
     _keyring_backend = keyring
 
 
