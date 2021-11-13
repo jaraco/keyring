cb300db9dc0a80fba386a009e756d30daf481fec
jrobbins-LiveData <jeffr@livedata.com>
2021-11-13 15:27:48 -0500

    Squashed commit of the following:
    commit 989412e2accddc28c4abbbacfeb12dfc4ef276e2
    Author: jrobbins-LiveData <jeffr@livedata.com>
    Date:   Sat Nov 13 12:50:01 2021 -0500
    
        simplified a bit
    
    commit fb37c365a042cfcb43c261f638bb59c3fbf8195f
    Author: jrobbins-LiveData <jeffr@livedata.com>
    Date:   Sat Nov 13 11:55:44 2021 -0500
    
        seems to work
    
    commit c63a8871d1bd90ce157dc9302d1e6646c43d528a
    Author: jrobbins-LiveData <jeffr@livedata.com>
    Date:   Sat Nov 13 11:38:53 2021 -0500
    
        _delete_password implemented
    
    commit c3de709f15b89fbe6a5dd4739aaf13f0d8343f3a
    Author: jrobbins-LiveData <jeffr@livedata.com>
    Date:   Sat Nov 13 11:08:44 2021 -0500
    
        checkpoint
    
    commit 06c41cd79c516d38a6eb34646197005cfd49f658
    Author: jrobbins-LiveData <jeffr@livedata.com>
    Date:   Sat Nov 13 10:02:29 2021 -0500
    
        initial commit testing 1280 Windows limit

--- keyring/backends/Windows.py
+++ keyring/backends/Windows.py
@@ -6,6 +6,9 @@ from ..backend import KeyringBackend
 from ..credentials import SimpleCredential
 from ..errors import PasswordDeleteError, ExceptionRaisedContext
 
+# NOTE: this can be increased if desired
+MAX_SHARDED_PASSWORD_LEN = 2 ** 16
+TARGET_SHARD = "{target}-shard-{i:04}"
 
 with ExceptionRaisedContext() as missing_deps:
     try:
@@ -95,6 +98,39 @@ class WinVaultKeyring(KeyringBackend):
     def _compound_name(username, service):
         return f'{username}@{service}'
 
+    def _discover_max_password(self):
+        def test_length(length):
+            try:
+                self._set_password("__probe_target__", "username", "a" * length)
+                self._delete_password("__probe_target__")
+                return True
+            except Exception as e:
+                if e.winerror == 1783 and e.funcname == 'CredWrite':
+                    return False
+                else:
+                    raise
+
+        # first try to confirm observed max length of 1280
+        if test_length(1280) and not test_length(1281):
+            return 1280
+
+        # otherwise resort to binary search
+        start, end = 1, MAX_SHARDED_PASSWORD_LEN
+        while start <= end:
+            mid = (start + end) // 2
+            if test_length(mid):
+                start = mid + 1
+            else:
+                end = mid - 1
+        return end
+
+    def __init__(self, *arg, **kw):
+        super().__init__(*arg, **kw)
+
+        # initial "super" max to prevent sharding during self._discover_max_password process
+        self._max_password = 2**32
+        self._max_password = self._discover_max_password()
+
     def get_password(self, service, username):
         # first attempt to get the password under the service name
         res = self._get_password(service)
@@ -105,7 +141,7 @@ class WinVaultKeyring(KeyringBackend):
             return None
         return res.value
 
-    def _get_password(self, target):
+    def _get_password_inner(self, target):
         try:
             res = win32cred.CredRead(
                 Type=win32cred.CRED_TYPE_GENERIC, TargetName=target
@@ -115,7 +151,21 @@ class WinVaultKeyring(KeyringBackend):
             if e.winerror == 1168 and e.funcname == 'CredRead':  # not found
                 return None
             raise
+
         return DecodingCredential(res)
+
+    def _get_password(self, target):
+        password = self._get_password_inner(target)
+
+        if password:
+            i = 1
+            while True:
+                shard = self._get_password_inner(TARGET_SHARD.format(target=target, i=i))
+                if not shard:
+                    return password
+                else:
+                    password["CredentialBlob"] += shard["CredentialBlob"]
+                    i += 1
 
     def set_password(self, service, username, password):
         existing_pw = self._get_password(service)
@@ -131,6 +181,30 @@ class WinVaultKeyring(KeyringBackend):
         self._set_password(service, username, str(password))
 
     def _set_password(self, target, username, password):
+        pwd_len = len(password)
+
+        if pwd_len > MAX_SHARDED_PASSWORD_LEN:
+            raise ValueError(f"_set_password: {pwd_len=} exceeds {MAX_SHARDED_PASSWORD_LEN=}")
+
+        if pwd_len > self._max_password:
+            n = self._max_password
+            max_shards = (pwd_len + n - 1) // n
+
+            # write all but the first shard
+            for i in range(1, max_shards):
+                shard = password[i * n: (i + 1) * n]
+                credential = dict(
+                    Type=win32cred.CRED_TYPE_GENERIC,
+                    TargetName=TARGET_SHARD.format(target=target, i=i),
+                    UserName=username,
+                    CredentialBlob=shard,
+                    Comment="Stored using python-keyring",
+                    Persist=self.persist,
+                )
+                win32cred.CredWrite(credential, 0)
+            # password set to first shard for "normal" writing
+            password = password[0: n]
+
         credential = dict(
             Type=win32cred.CRED_TYPE_GENERIC,
             TargetName=target,
@@ -152,14 +226,27 @@ class WinVaultKeyring(KeyringBackend):
         if not deleted:
             raise PasswordDeleteError(service)
 
-    def _delete_password(self, target):
+    def _delete_password_inner(self, target):
         try:
             win32cred.CredDelete(Type=win32cred.CRED_TYPE_GENERIC, TargetName=target)
+            return True
         except pywintypes.error as e:
             e = OldPywinError.wrap(e)
             if e.winerror == 1168 and e.funcname == 'CredDelete':  # not found
                 return
             raise
+
+    def _delete_password(self, target):
+        deleted = self._delete_password_inner(target)
+
+        if deleted:
+            i = 1
+            while True:
+                deleted = self._delete_password_inner(TARGET_SHARD.format(target=target, i=i))
+                if not deleted:
+                    return
+                else:
+                    i += 1
 
     def get_credential(self, service, username):
         res = None
--- tests/backends/test_Windows.py
+++ tests/backends/test_Windows.py
@@ -68,3 +68,39 @@ def test_winvault_always_viable():
     The WinVault backend should always be viable on Windows.
     """
     assert keyring.backends.Windows.WinVaultKeyring.viable
+
+
+@pytest.mark.skipif('sys.platform != "win32"')
+def test_long_password():
+    keyring_backend = keyring.backends.Windows.WinVaultKeyring()
+
+    max_password = keyring_backend._max_password
+
+    passwords = dict(
+        short_password='password',
+        almost_too_long_password='a' * max_password,
+        too_long_password='a' * (max_password + 1),
+        way_too_long_password='a' * 2**20
+    )
+
+    results = dict(
+        short_password=False,
+        almost_too_long_password=False,
+        too_long_password=False,
+        way_too_long_password=False
+    )
+
+    for test_case, password in passwords.items():
+        try:
+            keyring.set_password('__system__', test_case, password)
+            keyring.delete_password('__system__', test_case)
+            results[test_case] = True
+        except Exception as e:
+            if isinstance(e, ValueError) and test_case == 'way_too_long_password':
+                results[test_case] = True
+            elif e.winerror != 1783 or e.funcname != 'CredWrite' or test_case != 'too_long_password':
+                results[test_case] = False
+            else:
+                results[test_case] = True
+
+    assert all(result for result in results.values())
