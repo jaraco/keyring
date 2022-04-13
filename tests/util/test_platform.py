3692d64332768a6a8bd85ac5dbfecaba5c364d4a
Dos Moonen <d.moonen@nki.nl>
2022-04-13 16:37:03 +0200

    Use Pytest's skipif decorator instead of returning to skip assert statements.

--- tests/util/test_platform.py
+++ tests/util/test_platform.py
@@ -1,3 +1,4 @@
+import pytest
 import platform
 
 from keyring.util.platform_ import (
@@ -10,19 +11,21 @@ from keyring.util.platform_ import (
 )
 
 
+@pytest.mark.skipif(
+    platform.system() != "Linux", reason="Requires platform.system() == 'Linux'"
+)
 def test_platform_Linux():
     # rely on the Github Actions workflow to run this on different platforms
-    if platform.system() != "Linux":
-        return
 
     assert config_root == _config_root_Linux
     assert data_root == _data_root_Linux
 
 
+@pytest.mark.skipif(
+    platform.system() != "Windows", reason="Requires platform.system() == 'Windows'"
+)
 def test_platform_Windows():
     # rely on the Github Actions workflow to run this on different platforms
-    if platform.system() != "Windows":
-        return
 
     assert config_root == _config_root_Windows
     assert data_root == _data_root_Windows
