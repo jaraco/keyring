1054a531964472c95938eb2e2aa807c48eabf7f3
Jason R. Coombs <jaraco@jaraco.com>
2022-05-22 21:20:34 -0400

    Trim test down.

--- tests/util/test_platform.py
+++ tests/util/test_platform.py
@@ -1,31 +1,15 @@
 import pytest
-import platform
 
-from keyring.util.platform_ import (
-    config_root,
-    data_root,
-    _config_root_Linux,
-    _config_root_Windows,
-    _data_root_Linux,
-    _data_root_Windows,
-)
+from keyring.util import platform_
 
 
-@pytest.mark.skipif(
-    platform.system() != "Linux", reason="Requires platform.system() == 'Linux'"
-)
+@pytest.mark.skipif('platform.system() != "Linux"')
 def test_platform_Linux():
-    # rely on the Github Actions workflow to run this on different platforms
-
-    assert config_root == _config_root_Linux
-    assert data_root == _data_root_Linux
+    assert platform_.config_root == platform_._config_root_Linux
+    assert platform_.data_root == platform_._data_root_Linux
 
 
-@pytest.mark.skipif(
-    platform.system() != "Windows", reason="Requires platform.system() == 'Windows'"
-)
+@pytest.mark.skipif('platform.system() != "Windows"')
 def test_platform_Windows():
-    # rely on the Github Actions workflow to run this on different platforms
-
-    assert config_root == _config_root_Windows
-    assert data_root == _data_root_Windows
+    assert platform_.config_root == platform_._config_root_Windows
+    assert platform_.data_root == platform_._data_root_Windows
