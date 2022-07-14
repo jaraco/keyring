314c8d36fa06090938f95d2672316471b15a6e65
Jason R. Coombs <jaraco@jaraco.com>
2022-07-14 02:16:30 -0400

    Inline the suppress decorator from jaraco.context.

--- keyring/devpi_client.py
+++ keyring/devpi_client.py
@@ -9,7 +9,11 @@ from keyring.errors import KeyringError
 hookimpl = HookimplMarker("devpiclient")
 
 
+# https://github.com/jaraco/jaraco.context/blob/c3a9b739/jaraco/context.py#L205
+suppress = type('suppress', (contextlib.suppress, contextlib.ContextDecorator), {})
+
+
 @hookimpl()
+@suppress(KeyringError)
 def devpiclient_get_password(url, username):
-    with contextlib.suppress(KeyringError):
-        return keyring.get_password(url, username)
+    return keyring.get_password(url, username)
