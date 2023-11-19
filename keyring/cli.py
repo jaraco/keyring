c0df50c4bbd3a80a23fcd421baea6291fe88d934
Jason R. Coombs <jaraco@jaraco.com>
2023-11-19 18:48:37 -0500

    Fix implementation of strip_last_newline and added test.

--- keyring/cli.py
+++ keyring/cli.py
@@ -133,8 +133,15 @@ class CommandLineTool:
 
     @staticmethod
     def strip_last_newline(str):
-        """Strip one last newline, if present."""
-        return str[: -str.endswith('\n')]
+        r"""Strip one last newline, if present.
+
+        >>> CommandLineTool.strip_last_newline('foo')
+        'foo'
+        >>> CommandLineTool.strip_last_newline('foo\n')
+        'foo'
+        """
+        slc = slice(-1 if str.endswith('\n') else None)
+        return str[slc]
 
 
 def main(argv=None):
