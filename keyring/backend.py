e0015a482a6c8488efb9d4618c080fb2b7eaf03b
Jason R. Coombs <jaraco@jaraco.com>
2024-02-27 10:55:13 -0500

    👹 Feed the hobgoblins (delint).

--- keyring/backend.py
+++ keyring/backend.py
@@ -1,6 +1,7 @@
 """
 Keyring implementation support
 """
+
 from __future__ import annotations
 
 import os
--- keyring/backends/macOS/api.py
+++ keyring/backends/macOS/api.py
@@ -82,9 +82,9 @@ def create_query(**kwargs):
     return CFDictionaryCreate(
         None,
         (c_void_p * len(kwargs))(*[k_(k) for k in kwargs.keys()]),
-        (c_void_p * len(kwargs))(
-            *[create_cfstr(v) if isinstance(v, str) else v for v in kwargs.values()]
-        ),
+        (c_void_p * len(kwargs))(*[
+            create_cfstr(v) if isinstance(v, str) else v for v in kwargs.values()
+        ]),
         len(kwargs),
         _found.kCFTypeDictionaryKeyCallBacks,
         _found.kCFTypeDictionaryValueCallBacks,
