0025c77ba335777f9b022cb47db8f81df869c96b
Jason R. Coombs <jaraco@jaraco.com>
2024-03-22 11:14:46 -0400

    Removed check for config in XDG_DATA_HOME on Linux systems.
    Ref #99.

--- keyring/util/platform_.py
+++ keyring/util/platform_.py
@@ -30,33 +30,11 @@ def _data_root_Linux():
 _config_root_Windows = _data_root_Windows
 
 
-def _check_old_config_root():
-    """
-    Prior versions of keyring would search for the config
-    in XDG_DATA_HOME, but should probably have been
-    searching for config in XDG_CONFIG_HOME. If the
-    config exists in the former but not in the latter,
-    raise a RuntimeError to force the change.
-    """
-    # disable the check - once is enough and avoids infinite loop
-    globals()['_check_old_config_root'] = lambda: None
-    config_file_new = os.path.join(_config_root_Linux(), 'keyringrc.cfg')
-    config_file_old = os.path.join(_data_root_Linux(), 'keyringrc.cfg')
-    if os.path.isfile(config_file_old) and not os.path.isfile(config_file_new):
-        msg = (
-            "Keyring config exists only in the old location "
-            f"{config_file_old} and should be moved to {config_file_new} "
-            "to work with this version of keyring."
-        )
-        raise RuntimeError(msg)
-
-
 def _config_root_Linux():
     """
     Use freedesktop.org Base Dir Specification to determine config
     location.
     """
-    _check_old_config_root()
     fallback = pathlib.Path.home() / '.config'
     key = 'XDG_CONFIG_HOME'
     root = os.environ.get(key, None) or fallback
