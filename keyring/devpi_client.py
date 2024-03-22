8692ca29e6cc3e4e2ea0e738789568c7ca4a7c72
Jason R. Coombs <jaraco@jaraco.com>
2024-03-22 11:26:34 -0400

    Re-use 'suppress' from jaraco.context.

--- keyring/devpi_client.py
+++ keyring/devpi_client.py
@@ -1,15 +1,11 @@
-import contextlib
 import functools
 
 import pluggy
+from jaraco.context import suppress
 
 import keyring.errors
 
 hookimpl = pluggy.HookimplMarker("devpiclient")
-
-
-# https://github.com/jaraco/jaraco.context/blob/c3a9b739/jaraco/context.py#L205
-suppress = type('suppress', (contextlib.suppress, contextlib.ContextDecorator), {})
 
 
 def restore_signature(func):
