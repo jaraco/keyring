7881db629e13725fa19fa6aadc378acb18386a3e
Jason R. Coombs <jaraco@jaraco.com>
2024-03-23 09:40:37 -0400

    Remove ignore check for macOS API module now that compatible versions of Python 3.8 are in CI. Ref #547.

--- conftest.py
+++ conftest.py
@@ -1,21 +1,8 @@
-import ctypes
-
-collect_ignore = ["hook-keyring.backend.py"]
+import platform
 
 
-def macos_api_ignore():
-    """
-    Starting with macOS 11, the security API becomes
-    non-viable except on universal2 binaries.
+not_macOS = platform.system() != 'Darwin'
 
-    Ref #525.
-    """
-
-    try:
-        ctypes.CDLL(ctypes.util.find_library('Security')).SecItemAdd
-        return False
-    except Exception:
-        return True
-
-
-collect_ignore.extend(['keyring/backends/macOS/api.py'] * macos_api_ignore())
+collect_ignore = ["hook-keyring.backend.py"] + [
+    'keyring/backends/macOS/api.py'
+] * not_macOS
