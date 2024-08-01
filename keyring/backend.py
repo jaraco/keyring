18774025b3dca3d2f6edee4a10c423184ad956f2
Jason R. Coombs <jaraco@jaraco.com>
2024-08-01 13:12:25 -0400

    Extract method for registration.

--- keyring/backend.py
+++ keyring/backend.py
@@ -27,12 +27,16 @@ _limit: typing.Callable[[KeyringBackend], bool] | None = None
 
 class KeyringBackendMeta(abc.ABCMeta):
     """
-    A metaclass that's both an ABCMeta and a type that keeps a registry of
-    all (non-abstract) types.
+    Specialized subclass behavior.
+
+    Keeps a registry of all (non-abstract) types.
     """
 
     def __init__(cls, name, bases, dict):
         super().__init__(name, bases, dict)
+        cls._register()
+
+    def _register(cls):
         if not hasattr(cls, '_classes'):
             cls._classes = set()
         classes = cls._classes
